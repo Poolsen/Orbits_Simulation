d794b87079c95111a50cc8074b4d26eb4f32fe20
Paul-Keller03 <paul.ej.keller@outlook.com>
2024-10-27 20:19:45 +0100

    Revert "Dokumentation für Batch File erstellt"
    This reverts commit 7aacbe01b11a2762f7b25337127344df858b9ccc.

--- start_script.bat
+++ start_script.bat
@@ -1,7 +1,3 @@
-REM Startet das Venv, mithilfe eines Batch - Files und braucht somit keinen Interpreter.
-REM Sobald aber das Venv gestartet ist, kann main.py auf den mitgelieferten Interpreter zugreifen, da dieser im Venv steckt
-REM Am Ende wird das venv wieder geschlossen (nur für Logik, hat wahrscheinlich keinen praktischen Nutzen)
-
 @echo off
 cd /d %~dp0
 call venv\Scripts\activate
