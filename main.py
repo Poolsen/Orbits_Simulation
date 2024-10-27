2b3a0d43efb404bd4d228d3aa6a02d3bf5a00998
Paul-Keller03 <paul.ej.keller@outlook.com>
2024-10-27 20:16:37 +0100

    Window mit breite und hoehe in pygame erstellt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # installed packages: pygame
+from pygame.examples.moveit import WIDTH, HEIGHT
 
 # Path to your virtual environment
 venv_path = r"venv\Scripts\activate_this.py" #das ist der Pfad, um zu activate_this.py zu kommen und es dann auszuführen, was dann das venv startet --> packages verfügbar
@@ -8,10 +9,12 @@ with open(venv_path) as file_:
     exec(file_.read(), {'__file__': venv_path})
 
 import pygame       #nur um zu überprüfen, ob das venv wirklich gestartet wird: wenn nicht bricht das programm schon hier ab, da pygame nicht gefunden wird (nur im venv installiert)
+import math
 
-print("Hello World")
+pygame.init()
 
-n = 100
+Breite = 1000
+Laenge = 1000
 
-while n == 100:
-    n = input("n = ")
+Window = pygame.display.set_mode((Breite, Laenge))
+
