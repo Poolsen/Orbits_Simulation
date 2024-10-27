cf984c9c7df013b243dc5bd763f5943e0e960f77
Paul-Keller03 <paul.ej.keller@outlook.com>
2024-10-27 20:19:55 +0100

    Revert "Window mit breite und hoehe in pygame erstellt"
    This reverts commit 2b3a0d43efb404bd4d228d3aa6a02d3bf5a00998.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # installed packages: pygame
-from pygame.examples.moveit import WIDTH, HEIGHT
 
 # Path to your virtual environment
 venv_path = r"venv\Scripts\activate_this.py" #das ist der Pfad, um zu activate_this.py zu kommen und es dann auszuführen, was dann das venv startet --> packages verfügbar
@@ -9,12 +8,10 @@ with open(venv_path) as file_:
     exec(file_.read(), {'__file__': venv_path})
 
 import pygame       #nur um zu überprüfen, ob das venv wirklich gestartet wird: wenn nicht bricht das programm schon hier ab, da pygame nicht gefunden wird (nur im venv installiert)
-import math
 
-pygame.init()
+print("Hello World")
 
-Breite = 1000
-Laenge = 1000
+n = 100
 
-Window = pygame.display.set_mode((Breite, Laenge))
-
+while n == 100:
+    n = input("n = ")
