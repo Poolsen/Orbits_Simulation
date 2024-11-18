4563015de3e6b69eea2ac81a032b5c60ef967028
Paul-Keller03 <paul.ej.keller@outlook.com>
2024-11-18 14:51:31 +0100

    Geändert, dass kein echo bei REM

--- start_script.bat
+++ start_script.bat
@@ -1,8 +1,9 @@
+@echo off
+
 REM Startet das Venv, mithilfe eines Batch - Files und braucht somit keinen Interpreter.
 REM Sobald aber das Venv gestartet ist, kann main.py auf den mitgelieferten Interpreter zugreifen, da dieser im Venv steckt
 REM Am Ende wird das venv wieder geschlossen (nur für Logik, hat wahrscheinlich keinen praktischen Nutzen)
 
-@echo off
 cd /d %~dp0
 call venv\Scripts\activate
 python main.py
