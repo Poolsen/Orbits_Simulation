bb726425ed00c4ba11d4946a2ac10c32f2d8c69f
Paul-Keller03 <paul.ej.keller@outlook.com>
2024-12-21 17:56:22 +0100

    added __init__

--- Visuals/vis_Buttons.py
+++ Visuals/vis_Buttons.py
@@ -1,12 +1,21 @@
 class Button:
-    def __init__(self):
-        pass
-
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.shape = None
 
 
 class Rectangle(Button):
-    pass
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        pass
 
 
 class Circle(Button):
     pass
+
+class Cross(Button):
+    pass
+
+
