08c2c6e141c51467bcdd6a0b2207dcd0c091a71c
Paul-Keller03 <paul.ej.keller@outlook.com>
2025-01-20 21:43:14 +0100

    changed to import config instead of from config import * in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from config import *
+import config
 
 # initiates both physics and visual aspects of buttons and satellites, imports the function that decides what screen should be drawn
 from Visuals import init_buttons, init_satelliten, draw_current_screen
@@ -8,9 +8,9 @@ def main():
     satelliten = init_satelliten()
     buttons, buttons_update_pos = init_buttons()
 
-    draw_current_screen(satelliten, buttons, clock, "vis_Himmelskoerper", buttons_update_pos)       #der hier gepasste screen ist nur am Anfang, also mit was angefangen werden soll
+    draw_current_screen(satelliten, buttons, config.clock, "vis_Himmelskoerper", buttons_update_pos)       #der hier gepasste screen ist nur am Anfang, also mit was angefangen werden soll
 
-    pygame.quit()       # nachdem wir aus dem loop raus sind, soll auch pygame geschlossen werden (eigtl, sollte der code nie hier hinkommen, da schon jeweils bei der vis_draw_... Funktion geschlossen wird, aber als Backup )
-    sys.exit()
+    config.pygame.quit()       # nachdem wir aus dem loop raus sind, soll auch pygame geschlossen werden (eigtl, sollte der code nie hier hinkommen, da schon jeweils bei der vis_draw_... Funktion geschlossen wird, aber als Backup )
+    config.sys.exit()
 
 main()      #ich lasse die main() function laufen / ausführen (call)
