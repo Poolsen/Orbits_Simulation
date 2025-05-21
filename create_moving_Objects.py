6c33d37bcc90c2bc4dbd1099a474acad95c18050
Paul-Keller03 <paul.ej.keller@outlook.com>
2025-05-21 19:56:04 +0200

    added todos and Visual Object creation logic from Visuals package

--- create_moving_Objects.py
+++ create_moving_Objects.py
@@ -1,12 +1,19 @@
+#todo: Möglichkeit für importieren von vorgefertigten Szenarien importieren
+#todo: alten Code entfernen und durch create_moving_Objects ersetzen, damit code tatsächlich Funktionalität hat
+
 import config
 from Satellites_Calculations import MovingObject
+from Visuals import Visualisierung
 
-weiteres_Objekt: bool = True
-moving_objects = []
+weiteres_Objekt: bool = True        #kp warum, aber wenn man :bool entfernt kommt fucking type error
+moving_objects: list = []
+
+# nur temporary: irgendwie schlau ändern // edit: temporary mein ass
+print("Gib als erstes das Zentral - Objekt an, das du erstellen möchtest! Dies sollte das größte Objekt sein und erhält eine andere Farbe!")
+
 
 while weiteres_Objekt:
     name = input("Name: ")
-
     mass = input("Mass: ")
     x = input("X-Koordinate: ")
     y = input("Y-Koordinate: ")
@@ -16,14 +23,9 @@ while weiteres_Objekt:
 
     except ValueError as e:
         print(
-            f"Du muss eine Zahl mit Punkt als Komma eingeben! Sie darf keine Rechenoperation enthalten! Fehlercode: {e}")
+            f"Du muss eine Zahl mit *Punkt* als Komma eingeben! Sie darf keine Rechenoperation enthalten! Fehlercode: {e}")
     except Exception as e:
         print(f"Es gab einen unerwarteten, kritischen Fehler! Fehlercode {e}")
-
-
-    new_moving_object = MovingObject(x, y, mass)
-
-    moving_objects.append(new_moving_object)
 
     try:
         mass = float(mass)
@@ -33,14 +35,26 @@ while weiteres_Objekt:
     except Exception as e:
         print(f"Es gab einen unerwarteten, kritischen Fehler! Fehlercode {e}")
 
+    new_moving_object = MovingObject(x, y, mass)
+
+    if len(moving_objects) == 0:    # Zentral-Teil
+        new_Visual_object = Visualisierung(config.hellblau, 30)
+
+    else:
+        new_Visual_object = Visualisierung(config.weiss, 20)
+
+    moving_objects.append((new_moving_object, new_Visual_object)) # klammer wichtig, um als tuple zu passen
+
+
+
     if input("Drücke Enter für ein weiteres Objekt! ") == "":
         weiteres_Objekt = True
     else:
         weiteres_Objekt = False
 
-for n in range(len(moving_objects)):
+#Debugging - funktioniert: Es wird jedes Mal ein neues Objekt erstellt mit eigenen Attributen und Memory - Allocations
+"""for n in range(len(moving_objects)):
     print(moving_objects[n].x)
 for n in moving_objects:
-    print(n.x)
-
-# beide optionen möglich zum iteraten!
+    print(n.x)"""
+# beide optionen möglich zum Iterieren!
