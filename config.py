b94cb3233f0344c184193605bc9a9de84ab2d3a3
Paul-Keller03 <paul.ej.keller@outlook.com>
2025-06-14 13:48:04 +0200

    added color - option as a class for printing colored strings

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # definition aller variablen
 #constants
 
-FPS = 60    #maximale FPS, kann auch weniger sein, falls nicht genug computing ressources
+FPS = 60    #maximale FPS, kann auch weniger sein, falls nicht genug computing resources
 
 #screen to show
 #screen_to_show = "vis_Himmelskoerper"    #default
@@ -23,10 +23,45 @@ logarithmic_scroll: bool = True
 scale_divis = 1e5
 scale = 1 / scale_divis
 
+# laut dem tollen typen auf Stack Overflow kann man so farbigen output auch auf alten windows-kerneln erzeugen (:
+
+import os
+
+if os.name == 'nt':
+    import ctypes
+    kernel32 = ctypes.windll.kernel32
+    handle = kernel32.GetStdHandle(-11)  # STD_OUTPUT_HANDLE
+    mode = ctypes.c_uint32()
+    kernel32.GetConsoleMode(handle, ctypes.byref(mode))
+    mode.value |= 0x0004  # ENABLE_VIRTUAL_TERMINAL_PROCESSING
+    kernel32.SetConsoleMode(handle, mode)
+
+
+class Colors:
+    HEADER     = "\033[95m"
+    OKBLUE     = "\033[94m"
+    OKCYAN     = "\033[96m"
+    OKGREEN    = "\033[92m"
+    WARNING    = "\033[93m"
+    FAIL       = "\033[91m"
+    ENDC       = "\033[0m"
+    BOLD       = "\033[1m"
+    UNDERLINE  = "\033[4m"
+    BLACK      = "\033[30m"
+    RED        = "\033[31m"
+    ERROR      = "\033[31m"
+    GREEN      = "\033[32m"
+    SUCCESS    = "\033[32m"
+    YELLOW     = "\033[33m"
+    BLUE       = "\033[34m"
+    PURPLE     = "\033[35m"
+    CYAN       = "\033[36m"
+    WHITE      = "\033[37m"
+
 #screen to show
 
 #screen_to_show = "vis_Himmelskoerper"    #default
-"""
+""" -> verschoben nach launch < pygame_launcher
 screen = pygame.display.set_mode((breite, hoehe))   #window wird erstellt
 pygame.display.set_caption("Orbits_Simulation")     # Titel des Windows
 
